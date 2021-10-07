978b27cc9c074c5ed1179813d42498e4c7754274
Katey Harris <kateyharris9998@gmail.com>
2021-10-06 18:39:41 -0700

    Adding the election audit code.

--- Resources/PyPoll.py
+++ Resources/PyPoll.py
@@ -1,9 +1,154 @@
-# Import the datetime class from the datetime module.
-import datetime as dt
-# Use the now() attribute on the datetime class to get the present time.
-now = dt.datetime.now()
-# Print the present time.
-print("The time right now is ", now)
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Initialize a total vote counter.
+total_votes = 0
+# Candidate options and candidate votes
+candidate_options = []
+# 1. Declare the empty dictionary.
+candidate_votes = {}
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+# ## Total number of votes cast
+# # Open the election results and read the file
+# with open(file_to_load) as election_data:
+#     file_reader = csv.reader(election_data)
+#     # Read the header row.
+#     headers = next(file_reader)
+#     # Count each row in the CSV file using a loop +=1.
+#     for row in file_reader:
+#         # 2. Add to the total vote count.
+#         total_votes += 1
+# # 3. Print the total votes.
+# print(total_votes)
+
+# ## A complete list of candidates who received votes
+# # Open the election results and read the file.
+# with open(file_to_load) as election_data:
+#     file_reader = csv.reader(election_data)
+#     # Read the header row.
+#     headers = next(file_reader)
+#     # Print each row in the CSV file.
+#     for row in file_reader:
+#         # Add to the total vote count.
+#         total_votes += 1
+#         # Print the candidate name from each row.
+#         candidate_name = row[2]
+#         # If the candidate does not match any existing candidate...
+#         if candidate_name not in candidate_options:
+#             # Add it to the list of candidates.
+#             candidate_options.append(candidate_name)
+# # Print the candidate list.
+# print(candidate_options)
+
+# ## Total number of votes each candidate received
+# # Open the election results and read the file.
+# with open(file_to_load) as election_data:
+#     file_reader = csv.reader(election_data)
+#     # Read the header row.
+#     headers = next(file_reader)
+#     # Print each row in the CSV file.
+#     for row in file_reader:
+#         # Add to the total vote count.
+#         total_votes += 1
+#         # Print the candidate name from each row.
+#         candidate_name = row[2]
+#         if candidate_name not in candidate_options:
+#             # Add the candidate name to the candidate list.
+#             candidate_options.append(candidate_name)
+#            # Begin tracking that candidate's vote count.
+#             candidate_votes[candidate_name] = 0
+#         # Add a vote to that candidate's count
+#         candidate_votes[candidate_name] += 1
+# # Print the candidate vote dictionary.
+# print(candidate_votes)
+
+# ## Percentage of votes each candidate won
+# with open(file_to_load) as election_data:
+#     file_reader = csv.reader(election_data)
+#     # Read the header row.
+#     headers = next(file_reader)
+#     # Print each row in the CSV file.
+#     for row in file_reader:
+#         # Add to the total vote count.
+#         total_votes += 1
+#         # Print the candidate name from each row.
+#         candidate_name = row[2]
+#         if candidate_name not in candidate_options:
+#             # Add the candidate name to the candidate list.
+#             candidate_options.append(candidate_name)
+#            # Begin tracking that candidate's vote count.
+#             candidate_votes[candidate_name] = 0
+#         # Add a vote to that candidate's count
+#         candidate_votes[candidate_name] += 1
+# for candidate_name in candidate_votes:
+#     # 2. Retrieve vote count of a candidate.
+#     votes = candidate_votes[candidate_name]
+#     # 3. Calculate the percentage of votes.
+#     vote_percentage = float(votes) / float(total_votes) * 100
+#     # 4. Print the candidate name and percentage of votes.
+#     print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
+
+## The winner of the election based on popular vote
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+    # Read the header row.
+    headers = next(file_reader)
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # Add to the total vote count.
+        total_votes += 1
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            # Add the candidate name to the candidate list in if statement
+            candidate_options.append(candidate_name)
+            # Begin tracking that candidate's vote count in if statement
+            candidate_votes[candidate_name] = 0
+        # Add a vote to that candidate's count in the for loop
+        candidate_votes[candidate_name] += 1
+    # Determine the percentage of votes for each candidate by looping through the counts.
+    # Iterate through the candidate list.
+    for candidate_name in candidate_votes:
+        # Retrieve vote count of a candidate.
+        votes = candidate_votes[candidate_name]
+        # Calculate the percentage of votes.
+        vote_percentage = float(votes) / float(total_votes) * 100
+        # To do: print out each candidate's name, vote count, and percentage of
+        # votes to the terminal.
+        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        # Determine winning vote count and candidate
+        # Determine if the votes is greater than the winning count.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            # If true then set winning_count = votes and winning_percent =
+            # vote_percentage.
+            winning_count = votes
+            winning_percentage = vote_percentage
+            # And, set the winning_candidate equal to the candidate's name.
+            winning_candidate = candidate_name
+    winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+    print(winning_candidate_summary)
+
+# # Import the datetime class from the datetime module.
+# import datetime as dt
+# # Use the now() attribute on the datetime class to get the present time.
+# now = dt.datetime.now()
+# # Print the present time.
+# print("The time right now is ", now)
 
 # # Assign a variable for the file to load and the path. Direct Path
 # file_to_load = 'Resources/election_results.csv'
@@ -14,34 +159,9 @@ print("The time right now is ", now)
 #  # Close the file.
 # election_data.close()
 
-# Indirect Path Add our dependencies.
-import csv
-import os
-# Assign a variable to load a file from a path.
-file_to_load = os.path.join("Resources", "election_results.csv")
-# Assign a variable to save the file to a path.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
-
-# Open the election results and read the file.
-with open(file_to_load) as election_data:
-    file_reader = csv.reader(election_data)
-
-    # Read and print the header row. next() method will skip the first row and return the next item in the list.
-    headers = next(file_reader)
-    print(headers)
-
-
-
-
-# Using the with statement open the file as a text file.
-with open(file_to_save, "w") as txt_file:
-     # Write three counties to the file. \n mean new line
-     txt_file.write("Arapahoe\nDenver\nJefferson")
-# Close the file
-txt_file.close()
-
-# Total number of votes cast
-# A complete list of candidates who received votes
-# Total number of votes each candidate received
-# Percentage of votes each candidate won
-# The winner of the election based on popular vote
+# # Using the with statement open the file as a text file.
+# with open(file_to_save, "w") as txt_file:
+#      # Write three counties to the file. \n mean new line
+#      txt_file.write("Arapahoe\nDenver\nJefferson")
+# # Close the file
+# txt_file.close()
